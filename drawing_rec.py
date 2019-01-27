2301d62b63ff5e051de8c38841818b9a3c191dbe
Hannah <hannah.schlueter17@imperial.ac.uk>
2019-01-27 10:27:31 +0000

    wait for all 784 ints of image

--- drawing_rec.py
+++ drawing_rec.py
@@ -6,7 +6,7 @@ import time
 
 
 # import ML model
-model = load_model('ML/weights.h5')
+model = load_model('ML/weights_5.h5')
 buffer = np.array([]);
 
 HOST = ''                 # Symbolic name meaning all available interfaces
@@ -22,6 +22,9 @@ while True:
     data_array = np.fromstring(data, dtype=int, sep=' ')
     print("data shape", data_array.shape)
     buffer = np.append(buffer, data_array)
+    if buffer.size < 784:
+        print("waiting for more data :", buffer.size)
+        continue
     data_array = buffer[:784]
     buffer = buffer[784:]
     print(data_array)
@@ -32,11 +35,24 @@ while True:
     print("prediction ", prediction)
     y_classes = prediction.argmax(axis=-1)
     if prediction[0][y_classes[0]] > 0.5:
-        print('sent class')
+        print('sent class !')
+        # apple, banana, candle, fish, ladder
         conn.send(str.encode((str(y_classes[0]))))
+    elif prediction[0][y_classes[0]] > 0.30:
+        print('sent class ?')
+        # not so sure but maybe
+        # apple, banana, candle, fish, ladder
+        conn.send(str.encode((str(chr(y_classes[0] + 48 + 10)))))
     else:
         print('unknown')
         conn.send(str.encode('-'))
 conn.close()
 # optionally put a loop here so that you start
 # listening again after the connection closes
+
+
+
+
+
+
+# ['full_numpy_bitmap_apple', 'full_numpy_bitmap_banana', 'full_numpy_bitmap_bicycle', 'full_numpy_bitmap_broom', 'full_numpy_bitmap_butterfly', 'full_numpy_bitmap_candle', 'full_numpy_bitmap_cloud', 'full_numpy_bitmap_donut', 'full_numpy_bitmap_duck', 'full_numpy_bitmap_fish', 'full_numpy_bitmap_ladder', 'full_numpy_bitmap_rabbit', 'full_numpy_bitmap_snowflake']
